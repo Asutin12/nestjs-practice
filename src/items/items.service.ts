4db1d2cfd3ccf480d0c7e18a374a9259395a6c34
Asutin12 <asuto12271818@docomo.ne.jp>
2024-06-04 21:48:57 +0900

    create CRUD controller

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'this is ItemsService';
+  private items: Item[] = [];
+  findAll(): Item[] {
+    return this.items;
+  }
+  findByID(id: string): Item {
+    return this.items.find((i) => i.id === id);
+  }
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+  updateStatus(id: string): Item {
+    const item = this.findByID(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+  delete(id: string): void {
+    this.items = this.items.filter((i) => i.id !== id);
   }
 }
